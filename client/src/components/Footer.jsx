002d8ae59ff1ac24de61e5573b3b1fdb7d132d76
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-04-12 18:02:10 +1000

    added footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <h1>FOOOTER</h1>
+      </div>
+    </div>
+  );
+};
