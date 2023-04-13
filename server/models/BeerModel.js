592128f29e09d3bc1b89d642ce74a7bbd98acb7a
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-04-13 10:52:29 +1000

    updated beer schematics

--- server/models/BeerModel.js
+++ server/models/BeerModel.js
@@ -8,38 +8,50 @@ const beerSchema = new Schema(
       type: String,
       required: true,
     },
-    // variation: {
-    //     type: String,
-    //     required: false
-    // },
+    variation: {
+        type: String,
+        required: false
+    },
     origin: {
       type: String,
       required: true,
     },
-    // state: {
-    //     type: String,
-    //     required: false
-    // },
-    // image: {
-    //     type: String,
-    //     required: false
-    // },
+    state: {
+        type: String,
+        required: false
+    },
+    image_url: {
+        type: String,
+        required: true
+    },
     description: {
       type: String,
       required: true,
     },
-    // size: {
-    //     type: Number,
-    //     required: true
-    // },
-    // size_unit: {
-    //     type: String,
-    //     required: true
-    // },
-    // standard_drinks: {
-    //     type: Number,
-    //     required: true
-    // },
+    size: {
+        type: Number,
+        required: true
+    },
+    size_unit: {
+        type: String,
+        required: true
+    },
+    standard_drinks: {
+        type: Number,
+        required: true
+    },
+    alcohol_volume: {
+      type: String,
+      required: true
+    },
+    closure: {
+      type: String,
+      required: true
+    },
+    beer_style: {
+      type: String,
+      required: true
+    }
   },
   { timestamps: true }
 );
