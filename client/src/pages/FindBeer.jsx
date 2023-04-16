41a8d6b5b667af59dff67334c0ff0c3d6f2a8c78
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-04-16 15:57:23 +1000

    added fetch response for beers data

--- client/src/pages/FindBeer.jsx
+++ client/src/pages/FindBeer.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import BeerCard from "../components/BeerCard";
+import CountrySelector from "../components/CountrySelector";
 const FindBeer = () => {
+  const [beers, setBeers] = useState(null);
+
+  useEffect(() => {
+    const fetchBeers = async () => {
+      const response = await fetch("http://localhost:4000/beers");
+      const json = await response.json();
+
+      if (response.ok) {
+        setBeers(json);
+      }
+    };
+
+    fetchBeers();
+  }, []);
+
   return (
-    <div className="findbeer">
-      <h2>Find Beer</h2>
+    <div className="find-beer">
+      <div className="beers">
+        <CountrySelector beers={beers} />
+      </div>
     </div>
   );
 };
