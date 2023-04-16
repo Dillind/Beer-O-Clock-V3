8f4d4cc9e2d5bc62b048289924ec30f67c719fd2
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-04-16 15:58:55 +1000

    added home route and footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages & components
 import Home from "./pages/Home";
-import FindBeer from './pages/FindBeer'
+import FindBeer from "./pages/FindBeer";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import About from "./pages/About";
 
 function App() {
@@ -15,10 +16,11 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/findbeer" element={<FindBeer />} />
             <Route path="/about" element={<About />} />
+            <Route path="/findbeer" element={<FindBeer />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
