95ede0176ca2411bdff888acb0b67a53dce6afa7
Dylan Lindsay <dylan.lindsay234@gmail.com>
2023-04-26 15:57:21 +1000

    styling changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import BeerImage from "../images/beer-image-4.jpg";
 const About = () => {
   return (
-    <Grid container sx={{ height: "100vh" }}>
+    <Grid container sx={{ minHeight: "100vh" }}>
       <Grid
         container
         item
@@ -18,25 +18,27 @@ const About = () => {
           // backgroundAttachment: 'fixed'
         }}
       >
-        <Grid item xs={12} md={12} sx={{ ml: 5, pt: "100px", pb: "100px" }}>
+        <Grid item xs={12} sx={{ ml: 5, pt: "100px", pb: "100px" }}>
           <Typography
             sx={{
               color: "#fecc1a",
               borderBottom: "3px solid white",
               fontWeight: "800",
-              width: 190,
+              width: 200,
+              fontSize: "35px",
             }}
             variant="h4"
             component="h1"
           >
             ABOUT US.
           </Typography>
-          <Grid item xs={12} sx={{ mt: 3 }}>
+          <Grid item xs={12} sx={{ mt: 3, mb: 3 }}>
             <Typography
               sx={{
                 color: "#fff",
                 fontWeight: "700",
                 maxWidth: "50%",
+                fontSize: { xs: "16px", md: "20px" },
               }}
               variant="body1"
               component="p"
@@ -45,21 +47,6 @@ const About = () => {
               believe that everyone deserves to find their perfect pint, and we
               are here to help you do just that. Whether you're a beer
               connoisseur or just starting out, we've got you covered.
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography
-              variant="body1"
-              component="p"
-              sx={{
-                mt: 5,
-                mb: 4,
-                color: "#fff",
-                fontWeight: "800",
-                maxWidth: "50%",
-              }}
-            >
-              Click the button below to get started!
             </Typography>
           </Grid>
           <Grid item xs={12}>
@@ -73,6 +60,8 @@ const About = () => {
                 p: 1,
                 "&:hover": {
                   bgcolor: "#fecc1a",
+                  transform: "scale(1.05)",
+                  transition: "all 0.5s ease-in-out",
                 },
               }}
             >
